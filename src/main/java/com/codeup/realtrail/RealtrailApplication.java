99c2fdc3fc00dbba762f8afd33d073a07da079c1
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-06-18 09:41:55 -0500

    first commit with gitignore file and planning file

---
+++ src/main/java/com/codeup/realtrail/RealtrailApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.realtrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtrailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtrailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/realtrail/RealtrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.realtrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtrailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
