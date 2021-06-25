f5b9aee9eee5ef8c5644934d64effb57278370c4
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-06-25 15:22:52 -0500

    feat: working on saving profile image to db and fixed error messages

--- src/main/resources/static/js/fileStackImgUpload.js
+++ src/main/resources/static/js/fileStackImgUpload.js
@@ -1,16 +1,43 @@
  "use strict";
 
- const client = filestack.init(filestackapi);
- const options = {
-     accept: ["image/*"],
-     maxFiles: 1,
-     onUploadDone:
-     function (results){
-         console.log(results.filesUploaded[0].url);
-         $("#profileImageUrl").val(results.filesUploaded[0].url);
-     }
- }
+ $(document).ready(() => {
+     // click upload profile image
+     $("#upload").click((e) => {
+         e.preventDefault();
+         uploadProfileImage();
+     });
 
- $("#upload").click(() => {
-     client.picker(options).open();
+     let profileImageObj = {
+         profile_image_url: $("#profileImage").attr("src")
+     }
+     fetch("/profile/image", {
+         method: 'POST',
+         headers: {
+             'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(profileImageObj)
+     }).then( response => {
+         console.log(response);
+     }).catch( error => {
+         console.log(error);
+     });
  });
+
+ const client = filestack.init(filestackapi);
+ console.log(filestackapi);
+
+ function uploadProfileImage() {
+     const options = {
+         // accept: ["image/*"],
+         // maxFiles: 1,
+         onUploadDone: (results) => {
+             console.log(results.filesUploaded[0].url);
+             $("#profileImage").attr("src", results.filesUploaded[0].url);
+         },
+         onFileUploadFailed: (response) => {
+             console.log(response);
+         }
+     }
+
+     client.picker(options).open();
+ }
