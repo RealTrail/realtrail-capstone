fa149f5c2fa3eafbba9362e50f45326df4bdc8a0
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-06-25 19:58:48 -0500

    feat: cleaned css code

--- src/main/resources/static/js/fileStackImgUpload.js
+++ src/main/resources/static/js/fileStackImgUpload.js
@@ -1,31 +1,38 @@
  "use strict";
 
  $(document).ready(() => {
-     // click upload profile image
-     $("#upload").click((e) => {
-         e.preventDefault();
-         uploadProfileImage();
-         let profileImageObj = {
-             profile_image_url: $("#profileImageUrl").val()
-         }
-         fetch("/profile/image", {
-             method: 'POST',
-             headers: {
-                 'Content-Type': 'application/json'
-             },
-             body: JSON.stringify(profileImageObj)
-         }).then( response => {
-             console.log(response);
+     let isUploaded = false;
+     // user doesn't want to change profile image
+     if (isUploaded === false) {
+         $(".profileImageUrl").val($("#originalImage").val());
+     } else {
+         // click upload profile image
+         $("#upload").click((e) => {
+             e.preventDefault();
+             uploadProfileImage();
+             isUploaded = true;
+             let profileImageObj = {
+                 profile_image_url: $("#profileImageUrl").val()
+             }
+             fetch("/profile/image", {
+                 method: 'POST',
+                 headers: {
+                     'Content-Type': 'application/json'
+                 },
+                 body: JSON.stringify(profileImageObj)
+             }).then( response => {
+                 console.log(response);
 
-         }).catch( error => {
-             console.log("Error: ", error);
-             alert("Oops, something went wrong, cannot change profile image for now. Please try again later");
+             }).catch( error => {
+                 console.log("Error: ", error);
+                 alert("Oops, something went wrong, cannot change profile image for now. Please try again later");
+             });
          });
-     });
+     }
+
  });
 
  const client = filestack.init(filestackapi);
- console.log(filestackapi);
 
  function uploadProfileImage() {
      const options = {
@@ -34,7 +41,8 @@
          onUploadDone: (results) => {
              console.log(results.filesUploaded[0].url);
              $("#profileImage").attr("src", results.filesUploaded[0].url);
-             $("#profileImageUrl").val(results.filesUploaded[0].url);
+             $(".profileImageUrl").val(results.filesUploaded[0].url);
+             console.log($(".profileImageUrl").val());
          },
          onFileUploadFailed: (response) => {
              console.log(response);
