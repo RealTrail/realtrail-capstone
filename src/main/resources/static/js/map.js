e93ac954ad183f2624571e2d9379ea8fd9f07275
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-06-27 19:41:07 -0500

    feat: added map test

---
+++ src/main/resources/static/js/map.js
@@ -0,0 +1,9 @@
+"use strict";
+
+mapboxgl.accessToken = mapboxToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
+    center: [-98.491142, 29.424349], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
