26cd707ce0b77261deb24c7c893f0d7bd6ec95fe
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-07-01 17:50:30 -0500

    feat: Finished working on About Modal

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -12,7 +12,7 @@
 
 function globalNavDropdowns(e) {
     var t = this;
-    this.container = document.querySelector(e), this.root = this.container.querySelector(".navRoot"), this.primaryNav = this.root.querySelector(".navSection.primary"), this.primaryNavItem = this.root.querySelector(".navSection.primary .rootLink:last-child"), this.secondaryNavItem = this.root.querySelector(".navSection.secondary .rootLink:first-child"), this.checkCollision(), window.addEventListener("load", this.checkCollision.bind(this)), window.addEventListener("resize", this.checkCollision.bind(this)), this.container.classList.add("noDropdownTransition"), this.dropdownBackground = this.container.querySelector(".dropdownBackground"), this.dropdownBackgroundAlt = this.container.querySelector(".alternateBackground"), this.dropdownContainer = this.container.querySelector(".dropdownContainer"), this.dropdownArrow = this.container.querySelector(".dropdownArrow"), this.dropdownRoots = Strut.queryArray(".hasDropdown", this.root), this.dropdownSections = Strut.queryArray(".dropdownSection", this.container).map(function(e) {
+    this.container = document.querySelector(e), this.root = this.container.querySelector(".navRoot"), this.primaryNav = this.root.querySelector(".navSection.primary"), this.primaryNavItem = this.root.querySelector(".navSection.primary .rootLink:last-child"), this.secondaryNavItem = this.root.querySelector(".navSection.secondary .rootLink:first-child"), this.checkCollision(), window.addEventListener("load", this.checkCollision.bind(this)), window.addEventListener("resize", this.checkCollision.bind(this)), this.container.classList.add("noDropdownTransition"), this.dropdownBackground = this.container.querySelector(".dropdownBackground"), this.dropdownBackgroundAlt = this.container.querySelector(".alternateBackground"), this.dropdownContainer = this.container.querySelector(".dropdownContainer"), this.dropdownArrow = this.container.querySelector(".dropdownArrow"), this.dropdownRoots = Strut.queryArray(".hasDropdown", this.root), this.dropdownSections = Strut.queryArray(".dropdownSection", this.container).map(function (e) {
         return {
             el: e,
             name: e.getAttribute("data-dropdown"),
@@ -28,25 +28,25 @@ function globalNavDropdowns(e) {
         enter: "mouseenter",
         leave: "mouseleave"
     };
-    this.dropdownRoots.forEach(function(e, r) {
-        e.addEventListener(n.end, function(n) {
+    this.dropdownRoots.forEach(function (e, r) {
+        e.addEventListener(n.end, function (n) {
             n.preventDefault(), n.stopPropagation(), t.toggleDropdown(e)
-        }), e.addEventListener(n.enter, function(n) {
+        }), e.addEventListener(n.enter, function (n) {
             if (n.pointerType == "touch") return;
             t.stopCloseTimeout(), t.openDropdown(e)
-        }), e.addEventListener(n.leave, function(e) {
+        }), e.addEventListener(n.leave, function (e) {
             if (e.pointerType == "touch") return;
             t.startCloseTimeout()
         })
-    }), this.dropdownContainer.addEventListener(n.end, function(e) {
+    }), this.dropdownContainer.addEventListener(n.end, function (e) {
         e.stopPropagation()
-    }), this.dropdownContainer.addEventListener(n.enter, function(e) {
+    }), this.dropdownContainer.addEventListener(n.enter, function (e) {
         if (e.pointerType == "touch") return;
         t.stopCloseTimeout()
-    }), this.dropdownContainer.addEventListener(n.leave, function(e) {
+    }), this.dropdownContainer.addEventListener(n.leave, function (e) {
         if (e.pointerType == "touch") return;
         t.startCloseTimeout()
-    }), document.body.addEventListener(n.end, function(e) {
+    }), document.body.addEventListener(n.end, function (e) {
         Strut.touch.isDragging || t.closeDropdown()
     })
 }
@@ -54,24 +54,26 @@ function globalNavDropdowns(e) {
 function globalNavPopup(e) {
     var t = this,
         n = Strut.touch.isSupported ? "touchend" : "click";
-    this.activeClass = "globalPopupActive", this.root = document.querySelector(e), this.link = this.root.querySelector(".rootLink"), this.popup = this.root.querySelector(".popup"), this.closeButton = this.root.querySelector(".popupCloseButton"), this.link.addEventListener(n, function(e) {
+    this.activeClass = "globalPopupActive", this.root = document.querySelector(e), this.link = this.root.querySelector(".rootLink"), this.popup = this.root.querySelector(".popup"), this.closeButton = this.root.querySelector(".popupCloseButton"), this.link.addEventListener(n, function (e) {
         e.stopPropagation(), t.togglePopup()
-    }), this.popup.addEventListener(n, function(e) {
+    }), this.popup.addEventListener(n, function (e) {
         e.stopPropagation()
-    }), this.closeButton && this.closeButton.addEventListener(n, function(e) {
+    }), this.closeButton && this.closeButton.addEventListener(n, function (e) {
         t.closeAllPopups()
-    }), document.body.addEventListener(n, function(e) {
+    }), document.body.addEventListener(n, function (e) {
         Strut.touch.isDragging || t.closeAllPopups()
     }, !1)
-}(function() {
-    window.$ && window.$.ajaxPrefilter && $(function() {
-        return $.ajaxPrefilter(function(e, t, n) {
+}
+
+(function () {
+    window.$ && window.$.ajaxPrefilter && $(function () {
+        return $.ajaxPrefilter(function (e, t, n) {
             var r, i;
             return i = $("meta[name=csrf-token]"), r = i ? i.attr("content") : "", n.setRequestHeader("x-stripe-csrf-token", r)
         })
     })
 }).call(this),
-    function() {
+    function () {
         function i(e, t, n) {
             if (!("Analytics" in window)) return;
             n ? window.Analytics[e](t, {
@@ -80,85 +82,86 @@ function globalNavPopup(e) {
         }
 
         function s(e, t, n, r) {
-            e.addEventListener("click", function(e) {
+            e.addEventListener("click", function (e) {
                 i(t, n, r)
             })
         }
 
         function o() {
             var n = document.querySelectorAll("[" + e + "]");
-            [].slice.call(n).forEach(function(n) {
+            [].slice.call(n).forEach(function (n) {
                 s(n, "action", n.getAttribute(e), n.getAttribute(t))
             })
         }
 
         function u(e) {
             var t = document.querySelectorAll("[" + n + "]");
-            [].slice.call(t).forEach(function(e) {
+            [].slice.call(t).forEach(function (e) {
                 s(e, "modal", e.getAttribute(n), e.getAttribute(r))
             })
         }
+
         var e = "data-analytics-action",
             t = "data-action-source",
             n = "data-analytics-modal",
             r = "data-modal-source";
-        document.addEventListener("DOMContentLoaded", function() {
+        document.addEventListener("DOMContentLoaded", function () {
             o(), u()
         })
     }(), "use strict";
 var Strut = {
-    random: function(e, t) {
+    random: function (e, t) {
         return Math.random() * (t - e) + e
     },
-    arrayRandom: function(e) {
+    arrayRandom: function (e) {
         return e[Math.floor(Math.random() * e.length)]
     },
-    interpolate: function(e, t, n) {
+    interpolate: function (e, t, n) {
         return e * (1 - n) + t * n
     },
-    rangePosition: function(e, t, n) {
+    rangePosition: function (e, t, n) {
         return (n - e) / (t - e)
     },
-    clamp: function(e, t, n) {
+    clamp: function (e, t, n) {
         return Math.max(Math.min(e, n), t)
     },
-    queryArray: function(e, t) {
+    queryArray: function (e, t) {
         return t || (t = document.body), Array.prototype.slice.call(t.querySelectorAll(e))
     },
-    ready: function(e) {
+    ready: function (e) {
         document.readyState !== "loading" ? e() : document.addEventListener("DOMContentLoaded", e)
     }
 };
-Strut.isRetina = window.devicePixelRatio > 1.3, Strut.mobileViewportWidth = 670, Strut.isMobileViewport = window.innerWidth < Strut.mobileViewportWidth, window.addEventListener("resize", function() {
+Strut.isRetina = window.devicePixelRatio > 1.3, Strut.mobileViewportWidth = 670, Strut.isMobileViewport = window.innerWidth < Strut.mobileViewportWidth, window.addEventListener("resize", function () {
     Strut.isMobileViewport = window.innerWidth < Strut.mobileViewportWidth
 }), Strut.touch = {
     isSupported: "ontouchstart" in window || navigator.maxTouchPoints,
     isDragging: !1
-}, document.addEventListener("DOMContentLoaded", function() {
-    document.body.addEventListener("touchmove", function() {
+}, document.addEventListener("DOMContentLoaded", function () {
+    document.body.addEventListener("touchmove", function () {
         Strut.touch.isDragging = !0
-    }), document.body.addEventListener("touchstart", function() {
+    }), document.body.addEventListener("touchstart", function () {
         Strut.touch.isDragging = !1
     })
 }), Strut.load = {
-    images: function(e, t) {
+    images: function (e, t) {
         typeof e == "string" && (e = [e]);
         var n = -e.length;
-        e.forEach(function(e) {
+        e.forEach(function (e) {
             var r = new Image;
-            r.src = e, r.onload = function() {
+            r.src = e, r.onload = function () {
                 n++, n === 0 && t && t()
             }
         })
     },
-    css: function(e, t) {
+    css: function (e, t) {
         var n = document.createElement("link"),
             r = window.readConfig("strut_files") || {},
             i = r[e];
         if (!i) throw new Error('CSS file "' + e + '" not found in strut_files config');
         n.href = i, n.rel = "stylesheet", document.head.appendChild(n), t && (n.onload = t)
     },
-    js: function(e, t) {
+    js: function (e, t) {
         var n = document.createElement("script"),
             r = window.readConfig("strut_files") || {},
             i = r[e];
@@ -166,18 +169,18 @@ Strut.isRetina = window.devicePixelRatio > 1.3, Strut.mobileViewportWidth = 670,
         n.src = i, document.head.appendChild(n), t && (n.onload = t)
     }
 }, Strut.supports = {
-    es6: function() {
+    es6: function () {
         try {
             return new Function("(a = 0) => a"), !0
         } catch (e) {
             return !1
         }
     }(),
-    pointerEvents: function() {
+    pointerEvents: function () {
         var e = document.createElement("a").style;
         return e.cssText = "pointer-events:auto", e.pointerEvents === "auto"
     }(),
-    positionSticky: function() {
+    positionSticky: function () {
         var e = "position:",
             t = "sticky",
             n = document.createElement("a"),
@@ -185,10 +188,10 @@ Strut.isRetina = window.devicePixelRatio > 1.3, Strut.mobileViewportWidth = 670,
             i = " -webkit- -moz- -o- -ms- ".split(" ");
         return r.cssText = e + i.join(t + ";" + e).slice(0, -e.length), r.position.indexOf(t) !== -1
     }(),
-    masks: function() {
+    masks: function () {
         return !/MSIE|Trident|Edge/i.test(navigator.userAgent)
     }()
-}, globalNavDropdowns.prototype.checkCollision = function() {
+}, globalNavDropdowns.prototype.checkCollision = function () {
     var e = this;
     if (Strut.isMobileViewport) return;
     if (e.compact == 1) {
@@ -200,16 +203,16 @@ Strut.isRetina = window.devicePixelRatio > 1.3, Strut.mobileViewportWidth = 670,
             i = e.secondaryNavItem.getBoundingClientRect();
         r.right > i.left && (e.container.classList.add("compact"), e.compact = !0)
     }
-}, globalNavDropdowns.prototype.openDropdown = function(e) {
+}, globalNavDropdowns.prototype.openDropdown = function (e) {
     var t = this;
     if (this.activeDropdown === e) return;
-    this.container.classList.add("overlayActive"), this.container.classList.add("dropdownActive"), this.activeDropdown = e, this.dropdownRoots.forEach(function(e, t) {
+    this.container.classList.add("overlayActive"), this.container.classList.add("dropdownActive"), this.activeDropdown = e, this.dropdownRoots.forEach(function (e, t) {
         e.classList.remove("active")
     }), e.classList.add("active");
     var n = e.getAttribute("data-dropdown"),
         r = "left",
         i, s, o;
-    this.dropdownSections.forEach(function(e) {
+    this.dropdownSections.forEach(function (e) {
         e.el.classList.remove("active"), e.el.classList.remove("left"), e.el.classList.remove("right"), e.name == n ? (e.el.classList.add("active"), r = "right", i = e.content.offsetWidth, s = e.content.offsetHeight, o = e.content) : e.el.classList.add(r)
     });
     var u = 520,
@@ -218,37 +221,67 @@ Strut.isRetina = window.devicePixelRatio > 1.3, Strut.mobileViewportWidth = 670,
         l = s / a,
         c = e.getBoundingClientRect(),
         h = c.left + c.width / 2 - i / 2;
-    h = Math.round(Math.max(h, 10)), clearTimeout(this.disableTransitionTimeout), this.enableTransitionTimeout = setTimeout(function() {
+    h = Math.round(Math.max(h, 10)), clearTimeout(this.disableTransitionTimeout), this.enableTransitionTimeout = setTimeout(function () {
         t.container.classList.remove("noDropdownTransition")
     }, 50), this.dropdownBackground.style.transform = "translateX(" + h + "px) scaleX(" + f + ") scaleY(" + l + ")", this.dropdownContainer.style.transform = "translateX(" + h + "px)", this.dropdownContainer.style.width = i + "px", this.dropdownContainer.style.height = s + "px";
     var p = Math.round(c.left + c.width / 2);
     this.dropdownArrow.style.transform = "translateX(" + p + "px) rotate(45deg)";
     var d = o.children[0].offsetHeight / l;
     this.dropdownBackgroundAlt.style.transform = "translateY(" + d + "px)"
-}, globalNavDropdowns.prototype.closeDropdown = function() {
+}, globalNavDropdowns.prototype.closeDropdown = function () {
     var e = this;
     if (!this.activeDropdown) return;
-    this.dropdownRoots.forEach(function(e, t) {
+    this.dropdownRoots.forEach(function (e, t) {
         e.classList.remove("active")
-    }), clearTimeout(this.enableTransitionTimeout), this.disableTransitionTimeout = setTimeout(function() {
+    }), clearTimeout(this.enableTransitionTimeout), this.disableTransitionTimeout = setTimeout(function () {
         e.container.classList.add("noDropdownTransition")
     }, 50), this.container.classList.remove("overlayActive"), this.container.classList.remove("dropdownActive"), this.activeDropdown = undefined
-}, globalNavDropdowns.prototype.toggleDropdown = function(e) {
+}, globalNavDropdowns.prototype.toggleDropdown = function (e) {
     this.activeDropdown === e ? this.closeDropdown() : this.openDropdown(e)
-}, globalNavDropdowns.prototype.startCloseTimeout = function() {
+}, globalNavDropdowns.prototype.startCloseTimeout = function () {
     var e = this;
-    e.closeDropdownTimeout = setTimeout(function() {
+    e.closeDropdownTimeout = setTimeout(function () {
         e.closeDropdown()
     }, 50)
-}, globalNavDropdowns.prototype.stopCloseTimeout = function() {
+}, globalNavDropdowns.prototype.stopCloseTimeout = function () {
     var e = this;
     clearTimeout(e.closeDropdownTimeout)
-}, globalNavPopup.prototype.togglePopup = function() {
+}, globalNavPopup.prototype.togglePopup = function () {
     var e = this.root.classList.contains(this.activeClass);
     this.closeAllPopups(!0), e || this.root.classList.add(this.activeClass)
-}, globalNavPopup.prototype.closeAllPopups = function(e) {
+}, globalNavPopup.prototype.closeAllPopups = function (e) {
     var t = document.getElementsByClassName(this.activeClass);
     for (var n = 0; n < t.length; n++) t[n].classList.remove(this.activeClass)
-}, Strut.supports.pointerEvents || Strut.load.css("v3/shared/navigation_ie10.css"), Strut.ready(function() {
+}, Strut.supports.pointerEvents || Strut.load.css("v3/shared/navigation_ie10.css"), Strut.ready(function () {
     new globalNavDropdowns(".globalNav"), new globalNavPopup(".globalNav .navSection.mobile"), new globalNavPopup(".globalFooterNav .select.country"), new globalNavPopup(".globalFooterNav .select.language")
 });
+window.addEventListener('DOMContentLoaded', (event) => {
+    var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+
+    }
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
+
+// When the user clicks on <span> (x), close the modal
+
+
+// When the user clicks anywhere outside of the modal, close it
+
