08fddcabafba9ce2f13a0c341c8818e6c71a8d64
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-07-03 00:00:35 -0500

    feat: added trail creation method

--- src/main/java/com/codeup/realtrail/controllers/TrailController.java
+++ src/main/java/com/codeup/realtrail/controllers/TrailController.java
@@ -7,14 +7,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
 
 
 @Controller
 public class TrailController{
     private TrailsRepository trailsDao;
 
-
+    public TrailController(TrailsRepository trailsDao) {
+        this.trailsDao = trailsDao;
+    }
 
     @GetMapping("/trails/{id}")
     public String individualTrailPage(@PathVariable Long id, Model model){
@@ -23,6 +29,22 @@ public class TrailController{
         model.addAttribute("trail", trail);
 
         return "trails/showTrail";
+    }
+
+    @GetMapping("/trails/create")
+    public String getTrailForm(Model model, Principal principal) {
+        if (principal != null) {
+            model.addAttribute("trail", new Trail());
+            return "events/createEvent";
+        } else {
+            return "redirect:/login";
+        }
+    }
+
+    @PostMapping("/trails/create")
+    public String createTrail(@ModelAttribute Trail trail) {
+        trailsDao.save(trail);
+        return "events/createEvent";
     }
 
 }
