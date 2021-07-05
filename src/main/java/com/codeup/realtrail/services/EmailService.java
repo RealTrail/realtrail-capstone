b1bf0b76b9f0e0dd44bc23346cc50bdef20fadc9
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-07-04 23:46:31 -0500

    feat: recommiting

--- src/main/java/com/codeup/realtrail/services/EmailService.java
+++ src/main/java/com/codeup/realtrail/services/EmailService.java
@@ -1,6 +1,7 @@
 package com.codeup.realtrail.services;
 
 import com.codeup.realtrail.models.Event;
+import com.codeup.realtrail.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
@@ -29,6 +30,21 @@ public class EmailService {
         }
         catch (MailException ex) {
             // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
+
+    public void Send(User user, String subject, String body){
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo(user.getEmail());
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        try{
+            this.emailSender.send(msg);
+        }
+        catch(MailException ex){
             System.err.println(ex.getMessage());
         }
     }
