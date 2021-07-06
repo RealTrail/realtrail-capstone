b3cb6ce2930afdcafa57aad93af777dfd6d3663c
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-07-06 16:12:43 -0500

    profile settings

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -14,9 +14,9 @@ var check = function() {
     if (document.getElementById('createPassword').value ===
         document.getElementById('confirmCreatePassword').value) {
         document.getElementById('alertPasswordSign').style.color = '#8CC63E';
-        document.getElementById('alertPasswordSign').innerHTML = '<span><i class="fas fa-check-circle"></i>Match !</span>';
+        document.getElementById('alertPasswordSign').innerHTML = '<span class="loginspan"><i class="fas fa-check-circle"></i>Match !</span>';
     } else {
         document.getElementById('alertPasswordSign').style.color = '#EE2B39';
-        document.getElementById('alertPasswordSign').innerHTML = '<span><i class="fas fa-exclamation-triangle"></i>not matching</span>';
+        document.getElementById('alertPasswordSign').innerHTML = '<span class="loginspan"><i class="fas fa-exclamation-triangle"></i>not matching</span>';
     }
 }
