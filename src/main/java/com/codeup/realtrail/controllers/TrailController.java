fe850775f427eb6ea7275d97710ab3320b85f926
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-07-08 15:59:18 -0500

    feat: Finished event and trails  delete comments function

--- src/main/java/com/codeup/realtrail/controllers/TrailController.java
+++ src/main/java/com/codeup/realtrail/controllers/TrailController.java
@@ -4,13 +4,13 @@ import com.codeup.realtrail.daos.MapPointsRepository;
 import com.codeup.realtrail.daos.PictureURLsRepository;
 import com.codeup.realtrail.daos.TrailCommentsRepository;
 import com.codeup.realtrail.daos.TrailsRepository;
+import com.codeup.realtrail.daos.TrailCommentsRepository;
 import com.codeup.realtrail.models.*;
 import com.codeup.realtrail.services.UserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,21 +19,26 @@ import java.util.List;
 @Controller
 public class TrailController{
     private TrailsRepository trailsDao;
+    private TrailCommentsRepository trailCommentsDao;
     private PictureURLsRepository pictureURLSDao;
     private MapPointsRepository mapPointsDao;
     private UserService userService;
-    private TrailCommentsRepository trailCommentsDao;
+
+
 
     // importing mapbox token
     @Value("pk.eyJ1Ijoia2FjaGlrYWNoaWN1aSIsImEiOiJja25hanJ6ZnMwcHpnMnZtbDZ1MGh5dms1In0.JAsEFoNV2QP1XXVWXlfQxA")
     private String mapboxToken;
+    public TrailController(TrailsRepository trailsDao, TrailCommentsRepository trailCommentsDao, PictureURLsRepository pictureURLSDao, MapPointsRepository mapPointsDao, UserService userService) {
 
-    public TrailController(TrailsRepository trailsDao, PictureURLsRepository pictureURLSDao, MapPointsRepository mapPointsDao, UserService userService, TrailCommentsRepository trailCommentsDao) {
         this.trailsDao = trailsDao;
+        this.trailCommentsDao = trailCommentsDao;
         this.pictureURLSDao = pictureURLSDao;
         this.mapPointsDao = mapPointsDao;
         this.userService = userService;
-        this.trailCommentsDao = trailCommentsDao;
+
+
+
     }
 
     // showTrail.html- shows individual trail with all trail details
@@ -44,10 +49,14 @@ public class TrailController{
         TrailComment trailComment = new TrailComment();
         model.addAttribute("trailId", id);
         model.addAttribute("trail", trail);
+
         model.addAttribute("trailComment", trailComment);
         model.addAttribute("coordinates", coordinates);
         model.addAttribute("mapboxToken", mapboxToken);
         model.addAttribute("postUrl", "/trails/" + id + "/comment");
+
+        model.addAttribute("loggedInUser",userService.getLoggedInUser());
+
         return "trails/showTrail";
     }
 
@@ -68,6 +77,7 @@ public class TrailController{
 
         return trailSaved;
     }
+
 
     @PostMapping("/trails/{id}/comment")
     public String saveTrailComment(@PathVariable long id, @ModelAttribute TrailComment trailComment){
@@ -97,6 +107,17 @@ public class TrailController{
 
 //    @GetMapping("/searchCat")
 //    public
+
+
+    @PostMapping("/trails/{id}/comment/{cid}/delete")
+    public String deleteTrailComment(@PathVariable Long id, @PathVariable Long cid) {
+        User user =  userService.getLoggedInUser();
+        TrailComment trailcomment = trailCommentsDao.getById(cid);
+        if(user.isAdmin() || trailcomment.getOwner().getId()== user.getId()){
+            trailCommentsDao.deleteById(cid);
+        }
+        return "redirect:/trails/" + id;
+    }
 
 }
 
