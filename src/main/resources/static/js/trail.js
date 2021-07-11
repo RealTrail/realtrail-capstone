96c01114c84b19460555a02c0b4dc0db424913a3
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-07-11 05:37:37 -0500

    vertical mobile slide show

--- src/main/resources/static/js/trail.js
+++ src/main/resources/static/js/trail.js
@@ -1,35 +1,37 @@
 $(".slider")
     .slick({
+        infinite: true,
         slidesToShow: 6,
         slidesToScroll: 1,
-        arrows: true,
+        arrows: false,
+        autoplay: false,
         responsive: [
             {
-                breakpoint: 1665,
+                breakpoint: 1800,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: false
-                }
-            },
-            {
-                breakpoint: 1000,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 579,
-                settings: {
-                    slidesToShow: 1,
                     slidesToScroll: 1
                 }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 1008,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: "unslick"
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
+
         ]
     })
     .on("setPosition", function () {
