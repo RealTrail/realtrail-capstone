dfce120f8bf89f4301759d578c7eabe1dcb58c69
Victor G Hernandez <victor.g.hernandez87@gmail.com>
2021-07-12 08:39:56 -0500

    star rating

--- src/main/resources/static/js/trail.js
+++ src/main/resources/static/js/trail.js
@@ -85,27 +85,27 @@ function closeNav() {
 
 // Click function for show the Modal
 
-$("#myBtn, #myBtnM").on("click", function(){
-    $(".mask").addClass("active");
-});
-
-// Function for close the Modal
-
-function closeModal(){
-    $(".mask").removeClass("active");
-}
-
-// Call the closeModal function on the clicks/keyboard
-
-$(".close, .mask").on("click", function(){
-    closeModal();
-});
-
-$(document).keyup(function(e) {
-    if (e.keyCode === 27) {
-        closeModal();
-    }
-});
+// $("#myBtn, #myBtnM").on("click", function(){
+//     $(".mask").addClass("active");
+// });
+//
+// // Function for close the Modal
+//
+// function closeModal(){
+//     $(".mask").removeClass("active");
+// }
+//
+// // Call the closeModal function on the clicks/keyboard
+//
+// $(".close, .mask").on("click", function(){
+//     closeModal();
+// });
+//
+// $(document).keyup(function(e) {
+//     if (e.keyCode === 27) {
+//         closeModal();
+//     }
+// });
 
 
 // ******************** Trail Search ***************
