7c463d6b9725e3ae1f74545c4b2e680d0b279584
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-07-15 21:41:15 -0500

    feat: deleted admin reset user password option

--- src/main/java/com/codeup/realtrail/controllers/PasswordController.java
+++ src/main/java/com/codeup/realtrail/controllers/PasswordController.java
@@ -3,10 +3,12 @@ import com.codeup.realtrail.daos.UsersRepository;
 import com.codeup.realtrail.models.User;
 import com.codeup.realtrail.services.UserService;
 import com.codeup.realtrail.services.ValidationService;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,39 +34,6 @@ public class PasswordController {
 
     @PostMapping("/password/reset")
     public String resetPassword(@RequestParam(name = "oldPassword") String oldPassword,
-                                @RequestParam(name = "newPassword") String newPassword,
-                                @RequestParam(name = "confirmPassword") String confirmPassword,
-                                Model model){
-        User user = userService.getLoggedInUser();
-        String message = "";
-        boolean isPasswordSame = passwordEncoder.matches(oldPassword, user.getPassword());
-        if(!isPasswordSame){
-            message = "Password doesn't match. Please retry";
-            model.addAttribute("message", message);
-            return "users/resetPassword";
-        }else if(validationService.passwordHasError(newPassword) || validationService.passwordHasError(newPassword)){
-            message = "Password should be at least 8 digits long and must contain special characters";
-            model.addAttribute("passwordErrorMessage", message);
-            return "users/resetPassword";
-        }else if(!newPassword.equals(confirmPassword)){
-            message = "Not match";
-            model.addAttribute("passwordConfirmMessage", message);
-            return "users/resetPassword";
-        }else{
-            user.setPassword(passwordEncoder.encode(newPassword));
-            usersDao.save(user);
-            return "redirect:/login";
-        }
-    }
-
-
-    @GetMapping("/password/reset-admin")
-    public String getAdminResetUserPasswordForm() {
-        return "users/resetPassword";
-    }
-
-    @PostMapping("/password/reset-admin")
-    public String adminResetUserPassword(@RequestParam(name = "oldPassword") String oldPassword,
                                 @RequestParam(name = "newPassword") String newPassword,
                                 @RequestParam(name = "confirmPassword") String confirmPassword,
                                 Model model){
