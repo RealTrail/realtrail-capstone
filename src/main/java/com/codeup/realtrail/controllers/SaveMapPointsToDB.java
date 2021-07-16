56903d138a3c5a5a4efd08c8adfc956361012fa8
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-07-16 10:23:39 -0500

    feat: updated filestack API key and delete footer on some pages

--- src/main/java/com/codeup/realtrail/controllers/SaveMapPointsToDB.java
+++
@@ -1,9 +0,0 @@
-package com.codeup.realtrail.controllers;
-
-public class SaveMapPointsToDB {
-
-
-//    public static void main(String[] args) {
-//        mapPointsController.convertJsonToDB("/Users/shan/IdeaProjects/realtrail/data/government-canyon-state-natural-area.geojson");
-//    }
-}
