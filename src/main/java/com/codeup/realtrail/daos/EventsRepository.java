8341366d203f95c035d62f99ffc6f3f0cf5f3311
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-08-02 23:36:19 -0500

    feat: working on test for update event

--- src/main/java/com/codeup/realtrail/daos/EventsRepository.java
+++ src/main/java/com/codeup/realtrail/daos/EventsRepository.java
@@ -1,8 +1,12 @@
 package com.codeup.realtrail.daos;
 
 import com.codeup.realtrail.models.Event;
+import com.codeup.realtrail.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface EventsRepository extends JpaRepository <Event, Long> {
 
+    List<Event> getByOwner(User owner);
 }
